623b3dd354b48e52ca524fc84091c49a8086ee7b
Arw0 <ya6331856@gmail.com>
2026-09-04 22:31:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,19 +13,8 @@ import AdminLayout from "./pages/admin/AdminLayout";
 import Dashboard from "./pages/admin/Dashboard";
 import ApksPage from "./pages/admin/ApksPage";
 import FeaturedAppsPage from "./pages/admin/FeaturedAppsPage";
-import ReviewsPage from "./pages/admin/ReviewsPage";
-import FaqPage from "./pages/admin/FaqPage";
-import LiveWinnersPage from "./pages/admin/LiveWinnersPage";
-import RedeemCodesPage from "./pages/admin/RedeemCodesPage";
 import BlogPage from "./pages/admin/BlogPage";
-import SeoPage from "./pages/admin/SeoPage";
-import SeoDashboardPage from "./pages/admin/SeoDashboardPage";
-import AdsPage from "./pages/admin/AdsPage";
 import MediaLibraryPage from "./pages/admin/MediaLibraryPage";
-import NotificationsPage from "./pages/admin/NotificationsPage";
-import BackupPage from "./pages/admin/BackupPage";
-import SettingsPage from "./pages/admin/SettingsPage";
-import UsersSecurityPage from "./pages/admin/UsersSecurityPage";
 
 function LegacyAppRedirect() {
   const { id } = useParams();
@@ -48,19 +37,8 @@ function App() {
                   <Route path="dashboard" element={<Dashboard />} />
                   <Route path="apks" element={<ApksPage />} />
                   <Route path="featured-apps" element={<FeaturedAppsPage />} />
-                  <Route path="reviews" element={<ReviewsPage />} />
-                  <Route path="faq" element={<FaqPage />} />
-                  <Route path="live-winners" element={<LiveWinnersPage />} />
-                  <Route path="redeem-codes" element={<RedeemCodesPage />} />
                   <Route path="blog" element={<BlogPage />} />
-                  <Route path="seo" element={<SeoPage />} />
-                  <Route path="seo-dashboard" element={<SeoDashboardPage />} />
-                  <Route path="ads" element={<AdsPage />} />
                   <Route path="media-library" element={<MediaLibraryPage />} />
-                  <Route path="notifications" element={<NotificationsPage />} />
-                  <Route path="users" element={<UsersSecurityPage />} />
-                  <Route path="settings" element={<SettingsPage />} />
-                  <Route path="backup" element={<BackupPage />} />
                 </Route>
                 <Route path="/:slug" element={<AppDetail />} />
               </Routes>
