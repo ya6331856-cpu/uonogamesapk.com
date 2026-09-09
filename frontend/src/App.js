8a62f29a89c4413e6e97b82fb4a7db5f9eaa626d
Arw0 <ya6331856@gmail.com>
2026-09-09 16:07:36 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,12 +15,13 @@ import ApksPage from "./pages/admin/ApksPage";
 import FeaturedAppsPage from "./pages/admin/FeaturedAppsPage";
 import BlogPage from "./pages/admin/BlogPage";
 import MediaLibraryPage from "./pages/admin/MediaLibraryPage";
-
-// Naye missing pages (Spelling fixed with 's')
 import SeoDashboardPage from "./pages/admin/SeoDashboardPage";
 import BackupPage from "./pages/admin/BackupPage";
-import SettingsPage from "./pages/admin/SettingsPages"; 
 import UsersSecurityPage from "./pages/admin/UsersSecurityPages";
+import AdminReviews from "./components/admin/AdminReviews";
+
+// SettingsPages.jsx se saare sub-pages nikal kar import kar rahe hain
+import SettingsPage, { HomepagePage, CategoriesPage, AdsPage, NotificationsPage, HeroPage } from "./pages/admin/SettingsPages";
 
 function LegacyAppRedirect() {
   const { id } = useParams();
@@ -45,12 +46,18 @@ function App() {
                   <Route path="featured-apps" element={<FeaturedAppsPage />} />
                   <Route path="blog" element={<BlogPage />} />
                   <Route path="media-library" element={<MediaLibraryPage />} />
-                  
-                  {/* Naye routes yahan add kiye gaye hain */}
                   <Route path="seo-dashboard" element={<SeoDashboardPage />} />
                   <Route path="backup" element={<BackupPage />} />
-                  <Route path="settings" element={<SettingsPage />} />
                   <Route path="users" element={<UsersSecurityPage />} />
+                  
+                  {/* Yeh rahe aapke bache hue saare pages */}
+                  <Route path="settings" element={<SettingsPage />} />
+                  <Route path="homepage" element={<HomepagePage />} />
+                  <Route path="categories" element={<CategoriesPage />} />
+                  <Route path="ads" element={<AdsPage />} />
+                  <Route path="notifications" element={<NotificationsPage />} />
+                  <Route path="hero" element={<HeroPage />} />
+                  <Route path="reviews" element={<AdminReviews />} />
                 </Route>
                 <Route path="/:slug" element={<AppDetail />} />
               </Routes>
