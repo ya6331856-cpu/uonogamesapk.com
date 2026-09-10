a03331f13e0d282af6dcac2d4cbd898b6503c7da
Arw0 <ya6331856@gmail.com>
2026-09-11 01:29:19 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate, useParams } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import { Toaster } from "./components/ui/sonner";
+import { Toaster } from "sonner";
 import { AuthProvider } from "./context/AuthContext";
 import { SettingsProvider } from "./context/SettingsContext";
 import Store from "./pages/Store";
 import AppDetail from "./pages/AppDetail";
 import AdminLogin from "./pages/AdminLogin";
-import PWAInstallBanner from "./components/PWAInstallBanner";
+import PwaInstallBanner from "./components/PwaInstallBanner";
 import Analytics from "./components/Analytics";
+
+// Admin pages & layouts
 import AdminLayout from "./pages/admin/AdminLayout";
 import Dashboard from "./pages/admin/Dashboard";
 import ApksPage from "./pages/admin/ApksPage";
@@ -17,11 +19,9 @@ import BlogPage from "./pages/admin/BlogPage";
 import MediaLibraryPage from "./pages/admin/MediaLibraryPage";
 import SeoDashboardPage from "./pages/admin/SeoDashboardPage";
 import BackupPage from "./pages/admin/BackupPage";
-import UsersSecurityPage from "./pages/admin/UsersSecurityPages";
+import UsersSecurityPage from "./pages/admin/UsersSecurityPage";
 import AdminReviews from "./components/admin/AdminReviews";
-
-// SettingsPages.jsx se saare sub-pages aur main settings import ki gayi hai
-import SettingsPage, { HomepagePage, CategoriesPage, AdsPage, NotificationsPage, HeroPage } from "./pages/admin/SettingsPages";
+import SettingsPage, { HomepagePage, CategoriesPage, AdsPage, NotificationsPage, HeroPage } from "./pages/admin/SettingsPage";
 
 function LegacyAppRedirect() {
   const { id } = useParams();
@@ -39,6 +39,7 @@ function App() {
                 <Route path="/" element={<Store />} />
                 <Route path="/app/:id" element={<LegacyAppRedirect />} />
                 <Route path="/admin/login" element={<AdminLogin />} />
+                
                 <Route path="/admin" element={<AdminLayout />}>
                   <Route index element={<Navigate to="/admin/dashboard" replace />} />
                   <Route path="dashboard" element={<Dashboard />} />
@@ -46,15 +47,10 @@ function App() {
                   <Route path="featured-apps" element={<FeaturedAppsPage />} />
                   <Route path="blog" element={<BlogPage />} />
                   <Route path="media-library" element={<MediaLibraryPage />} />
-                  
-                  {/* SEO routes dono formats ke liye handle kiye gaye hain */}
                   <Route path="seo" element={<SeoDashboardPage />} />
                   <Route path="seo-dashboard" element={<SeoDashboardPage />} />
-                  
                   <Route path="backup" element={<BackupPage />} />
                   <Route path="users" element={<UsersSecurityPage />} />
-                  
-                  {/* Settings aur baaki sabhi sub-pages */}
                   <Route path="settings" element={<SettingsPage />} />
                   <Route path="homepage" element={<HomepagePage />} />
                   <Route path="categories" element={<CategoriesPage />} />
@@ -63,10 +59,11 @@ function App() {
                   <Route path="hero" element={<HeroPage />} />
                   <Route path="reviews" element={<AdminReviews />} />
                 </Route>
+
                 <Route path="/:slug" element={<AppDetail />} />
               </Routes>
             </BrowserRouter>
-            <PWAInstallBanner />
+            <PwaInstallBanner />
             <Analytics />
             <Toaster position="bottom-center" offset={30} richColors />
           </AuthProvider>
