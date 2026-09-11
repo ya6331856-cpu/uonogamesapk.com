acf7e26b7539b180940fdd0cfd5dd99c3da7d6dc
Arw0 <ya6331856@gmail.com>
2026-09-11 17:20:23 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -55,48 +55,7 @@ logger = logging.getLogger(__name__)
 # ---------------------------------------------------------------------------
 # Google Indexing API Setup
 # ---------------------------------------------------------------------------
-INDEXING_SCOPES = ["https://www.googleapis.com/auth/indexing"]
-
-SERVICE_ACCOUNT_INFO = {
-    "type": "service_account",
-    "project_id": "uonogamesapk",
-    "private_key_id": "f02c6f04e58802161f3b5e5b251fe7f22a016ae6",
-    "private_key": """-----BEGIN PRIVATE KEY-----
-MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDlmMpOEoA6dq5E
-SD+YPlgg/5OY4nGFAUXEZPRDJohhWVQuqwt8cFloYM3k1RF+EbPYRye9hFtKpKz0
-Ewbdh7TQ8EN/VMvPwI2wsSotBf6rNOmPFQha4pHNxlZi+2X7Yp21YRtR3a0W8Nya
-xlk+0cHKwooa03f3os+p36l0nMY3QtqWsXxGTSpzXTnuCwrYTZOOGM8LeWHQDtVj
-SzF2FLkd7tMH4IcrdxkpRrZ5lH+bybScvgWNjExVsLVlxLSDU+0YfkO+FMPGI1x3
-GfxVx6g7uIioJ0aeV7LcTGmG1pYDgR+rkWCefQTxshTp2l/M8U+DqOhVQgZ0w/tZ
-qMwX6tUpAgMBAAECggEARBXPpGBLI0sveXM9XJ3cT4jK0bMQ++K5ODjB0Kn8FnZB
-hwdVBCVi9heC5yGiKtMAFJphOAuocNWtrkF4Lzh/j9g4A+n2+Jg+zE91VN2qvEWY
-H0Sa8iyvFSkEVBz+DJKddvyhd+D3Y86wdLtuGrlkMQcIolTkhgyDbXo0i66z/w2l
-xqXmj3mIkWPYZMJuVD/hzzVBCY+B8N/+NMTUuX07U0msUJsve/ResFIZunvUVaxr
-ya81wDZIhUkvXXDL45xf+JRJrpG23/jbDK7jCkXX8aa+J2KDSwPwOmLwYVdeAPc9
-VZqP+yO2KLojzSr1Xmbi10IN26HZ8XZE7d22eKlsswKBgQD+KTEu3Csmw8y7Q8ZD
-zD56XlbQBAFod/8BZHd4FvKScRidwhICZdxXQS+3XvQQUei9J2kLhFcZQpZshAIy
-3zhJY3RmThttK9I/xBaEy/ZyEDOTcNDoTUilxKXvfWcN+UFxn9jhVjPYFZrYRyRF
-eQHMmrUJRCy7q2jgIwlWNTCijwKBgQDnQhh7IZdIFbK7myfUdEvgE4I2Q4ISFwlx
-6p65jvMxmEf6KgoH1FBAjqUjyBZdDSFyQCKRC8T3l/ADJt+eqULx61GcpNzcNWFB
-MWrO9+hTjsw0Ifgu3M1iHcRGnv++1UJlYcwkjfBxEUb11GGYMUaILwL95qbmQBHe
-cMOkCXgIxwKBgBpGOd5lRlS4kxac2Ac0OxU9YW4Zq+eX2BXVw//3J1Z6OJg+cswq
-qY+fnoYvW73AKfY798EIClUDLDfFodCOgOwdSvA0jONJT2/mHonV6AE8qYhJdl89
-dhAk9x598URhiyFq6+nHlo51FU/ccuR3sPbs22A82v7/plTdal6uGvwDAoGAbP85
-HKfrbr1TXZs2fatGq9lmEP9mifIzsG5920WmGCUHH8C6s4/9N0BEU4YWDEuJDRlv
-cV/TuULyi/nBgj2S4QUhlSwbMOsz6I9LITu1U9TFKHkuSaAmaW1QOlzse1x2i+Q5
-XK1Nu20CPhGY4iuva7aEuXkCBxoBkg8iFumjmrcCgYEA6sKEvjaiA7sJkRYl+EDK
-BSEKulFDuKLuCh6pAMowV7+mfBLH9qqBQ02+8uQSAAeNnyQqu68OHQRto7oVF93o
-3kryvynr73KIHrPpRMR+mvQwDwtkPv4FRn6kWESVw57g+QLByc79Wk8X4TjPZeAJ
-hgfLW13lSv2NN2CFKi/cRbE=
------END PRIVATE KEY-----""",
-    "client_email": "newyono-indexer@uonogamesapk.iam.gserviceaccount.com",
-    "client_id": "112018094980782324050",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/newyono-indexer%40uonogamesapk.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
-}
+SERVICE_ACCOUNT_INFO = json.loads(os.environ.get("GOOGLE_SERVICE_ACCOUNT_JSON", "{}"))
 
 def _notify_single_url(slug: str):
     target_url = f"https://newyono.games/{slug}"
